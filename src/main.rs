9bd0937b6eff567b089b5dcdc0a95c73c5b5855f
smokytheangel0 <j.m.a.kierstead@gmail.com>
2018-08-21 15:06:12 -0700

    fooling around with inline docs, very commendable

--- src/main.rs
+++ src/main.rs
@@ -12,21 +12,80 @@
 
 use std::env;
 
+///the [check_dirs] function looks like this
+/// in python:
+/// ```python
+/// def check_dirs(errorBOX):
+///     pathBOX = os.getcwd()
+/// 
+///     if "Downloads" not in initial_directory:
+///         print(errorBOX)
+///         sys.exit()
+/// ```
+/// in dart:
+/// ```dart
+/// // to be implemented!
+/// ```
 fn check_dirs() -> i8 {
     //this works in Mac, Linux and Windows
     let mut outBOX = 0;
 
-    let path = env::current_dir().ok().unwrap();
-    let initialDirectory = path.to_str().unwrap();
+    let pathBuffer = env::current_dir().ok().unwrap();
+    let pathString = pathBuffer.to_str().unwrap();
 
     let errorBOX = String::from("This program you've just run does not appear to be in the Downloads folder, please try running it again with it in the Downloads folder");
     
-    if !initialDirectory.contains("Downloads") {
+    if pathString.contains("Downloads") == false {
         println!("{}", errorBOX);
         outBOX += 1;
     }
     outBOX
 }
+
+///the [start_downloads] function looks like this
+/// in python:
+/// ```python
+/// def start_downloads(errorBOX):
+///     system = platform.uname()[0]
+///     
+///     if system == "Linux":
+///         vs_version = "linux64_deb"
+///         extension = "zip"
+///         #git is installed using apt on ubuntu
+/// 
+///     if system == "Windows":
+///         vs_version = "win32"
+///         extension = "exe"
+///         git_url = "https://github.com/git-for-windows/git/releases/download/v2.18.0.windows.1/Git-2.18.0-64-bit.exe"
+/// 
+///     if system == "Darwin":
+///         vs_version = "osx"
+///         extension = "dmg"
+///         git_url = "https://sourceforge.net/projects/git-osx-installer/files/git-2.18.0-intel-universal-mavericks.dmg/download?use_mirror=autoselect"
+/// 
+///     url = "https://github.com/smokytheangel0/co_demo0/"
+///     try:
+///         webbrowser.open(url, new=0, autoraise=True)
+///     except:
+///         for line in errorBOX:
+///             if line == errorBOX[0]:
+///                 line = errorBOX[0].split()
+///                 line[1] = "co_demo"
+///                 line = " ".join(line)
+///             print(line)
+///         sys.exit()
+/// 
+///     url = "https://github.com/flutter/flutter/archive/master.zip"
+///     try:
+///         webbrowser.open(url, new=0, autoraise=True)
+///     except:
+///         for line in errorBOX:
+///             if line == errorBOX[0]:
+///                 line = errorBOX[0].split()
+///                 line[1] = "flutter"
+///                 line = " ".join(line)
+///             print(line)
+/// ...To Be Continued...
 
 fn start_downloads() -> String {
     let errorBOX = String::from("The __None_ download failed, please try running the program again to try again");
