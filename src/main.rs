b0bb03a116b569bb9d1ab8ac8ca18a6ad643b899
smokytheangel0 <j.m.a.kierstead@gmail.com>
2018-10-21 13:14:48 -0700

    added bash command to refresh path, . not found after spawning and i must remember spawns capture and print the output

--- src/main.rs
+++ src/main.rs
@@ -915,7 +915,7 @@ fn set_path() {
         writeln!(fileBOX, "export PATH=$HOME/Desktop/SDKs/flutter/bin:$PATH").expect("failed to write unix flutter path");
         writeln!(fileBOX, "export PATH=$ANDROID_HOME/tools:$PATH").expect("failed to write unix tools path");
         writeln!(fileBOX, "export PATH=$ANDROID_HOME/platform-tools:$PATH").expect("failed to write unix platform tools path");
-        Command::new("sudo").arg(".").arg(&homePATH).spawn().expect("failed to refresh bash_profile");
+        Command::new("sudo").argo("bash").arg(".").arg(&homePATH).spawn().expect("failed to refresh bash_profile");
     }
 
     #[cfg(windows)]
