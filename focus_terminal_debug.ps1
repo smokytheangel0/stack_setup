45cf6fb275d4ce59773beac662bd25c415d33ed3
Justice Kierstead <j.m.a.kierstead@gmail.com>
2018-10-31 17:01:33 -0700

    change ps1 files to match casing

--- focus_terminal_debug.ps1
+++ focus_terminal_debug.ps1
@@ -1,11 +1,13 @@
 Add-Type @"
-    using System;
-    using System.Runtime.InteropServices;
-    public class SFW {
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool SetForegroundWindow(IntPtr hWnd);
-    }
+  using System;
+  using System.Runtime.InteropServices;
+  public class SFW {
+     [DllImport("user32.dll")]
+     [return: MarshalAs(UnmanagedType.Bool)]
+     public static extern bool SetForegroundWindow(IntPtr hWnd);
+  }
 "@
-$h = (Get-Process powershell).MainWindowHandle
+
+
+$h =  (get-process powershell).MainWindowHandle
 [SFW]::SetForegroundWindow($h)
